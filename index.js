e7fea9e665d850886aff4d1c492d6f1dfc7ce32c
Choharim <jhl8586@naver.com>
2020-11-25 13:03:10 +0900

    Add section1,2

--- index.js
+++ index.js
@@ -5,6 +5,13 @@ function smoothScroll(target,duration){
   let distance = targetPostion - startPosition;
   let startTime = null;
 
+  function ease(t,b,c,d){
+    t /=d /2 ;
+    if(t<1)return c/2*t*t+b;
+    t--;
+    return -c /2*(t*(t-2)-1)+b;
+  }
+
   function animation(currentTime){
     if(startTime === null) startTime = currentTime;
       let timeElapsed = currentTime - startTime;
@@ -13,18 +20,17 @@ function smoothScroll(target,duration){
       if(timeElapsed < duration) requestAnimationFrame(animation);
   }
 
-  function ease(t,b,c,d){
-    t /=d /2 ;
-    if(t<1)return c/2*t*t+b;
-    t--;
-    return -c /2*(t*(t-2)-1)+b;
-  }
   requestAnimationFrame(animation);
 }
 
 
 
 let section1 = document.querySelector(".section1");
+let section2 = document.querySelector(".section2");
+
 section1.addEventListener("click",function(){
   smoothScroll(".section2",1000);
+});
+section2.addEventListener("click",function(){
+  smoothScroll(".section1",1000);
 });
